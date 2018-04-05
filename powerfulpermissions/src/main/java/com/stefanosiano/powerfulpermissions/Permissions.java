a05e5869bdddc39739b4c4eed47a85c4503b4e3c
stefano <ssiano@zerodo.it>
2018-04-05 12:21:54 +0200

    very first version of annotation and library

--- powerfulpermissions/src/main/java/com/stefanosiano/powerfulpermissions/Permissions.java
+++ powerfulpermissions/src/main/java/com/stefanosiano/powerfulpermissions/Permissions.java
@@ -1,38 +1,72 @@
 package com.stefanosiano.powerfulpermissions;
 
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.util.SparseArray;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Permissions {
 
-    public static boolean check(){
+    private static boolean initialized = false;
+    static Map<String, SparseArray<ContextPermMapping>> activitiesMap = new HashMap<>();
+
+    public static void init() {
+        initialized = false;
+        activitiesMap = new HashMap<>();
+    }
+
+    private static void initActivityMap(){
+        if(!initialized){
+            try {
+                Class permissionPPClass = ClassLoader.getSystemClassLoader().loadClass("Permissions$PowerfulPermission");
+                permissionPPClass.getDeclaredMethod("init", Map.class).invoke(null, activitiesMap);
+                initialized = true;
+            }
+            catch (Exception e){
+                new RuntimeException("Unable to initialize the activity-permissions mapping");
+            }
+        }
+    }
+
+    public static boolean check(Context context){
+        initialized = true;
+        initActivityMap();
+        SparseArray<ContextPermMapping> contextPermMappings = activitiesMap.get(context.getClass().getName());
+
+        if(contextPermMappings == null) throw new RuntimeException("Trying to call a method not annotated");
+
+        String methodName = new Throwable().getStackTrace()[1].getMethodName();
+        ContextPermMapping contextPermMapping = ContextPermMappingFinder.findByMethod(methodName, contextPermMappings);
+
+        if(contextPermMapping == null) throw new RuntimeException("Trying to call a method without permissions in annotation");
+
+        for (String perm : contextPermMapping.permissions) {
+            if (ContextCompat.checkSelfPermission(context, perm) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
         return true;
     }
 
 
-    private static final int MAX_LOG_LENGTH = 4000;
-    private static final int MAX_TAG_LENGTH = 23;
-    private static final int CALL_STACK_INDEX = 5;
-    private static final Pattern ANONYMOUS_CLASS = Pattern.compile("(\\$\\d+)+$");
-
-    public void a() {
-        getElement().get
-    }
-
-
-    final StackTraceElement getElement() {
-
-        // DO NOT switch this to Thread.getCurrentThread().getStackTrace(). The test will pass
-        // because Robolectric runs them on the JVM but on Android the elements are different.
-        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-        if (stackTrace.length <= CALL_STACK_INDEX) {
-            throw new IllegalStateException(
-                    "Synthetic stacktrace didn't have enough elements: are you using proguard?");
+    public static class ContextPermMappingFinder {
+        public static ContextPermMapping findByMethod(String methodName, SparseArray<ContextPermMapping> contextPermMappingSparseArray){
+            for(int i = 0 ; i < contextPermMappingSparseArray.size(); i++){
+                if(contextPermMappingSparseArray.get(i).methodName.equals(methodName))
+                    return contextPermMappingSparseArray.get(i);
+            }
+            return null;
         }
-        return stackTrace[CALL_STACK_INDEX];
     }
 
+    public class ContextPermMapping {
+        String[] permissions;
+        String methodName;
+        int methodId;
+    }
 }
