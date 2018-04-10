d3f58785b81b7af20e63cf174ff6b3780e74a8c0
stefano <ssiano@zerodo.it>
2018-04-10 13:23:34 +0200

    another try of the library

---
+++ powerfulpermissions-processor/src/main/java/com/stefanosiano/powerfulpermissions/processor/annotation/PermMapping.java
@@ -0,0 +1,13 @@
+package com.stefanosiano.powerfulpermissions.annotation;
+
+public class PermMapping {
+    public String[] permissions;
+    public String methodName;
+    public int methodId;
+
+    public PermMapping(String[] permissions, String methodName, int methodId) {
+        this.permissions = permissions;
+        this.methodName = methodName;
+        this.methodId = methodId;
+    }
+}
